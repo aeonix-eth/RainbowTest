3cd8ee5c118d1f10b6340198384895e2ac439714
aeonix-eth <a17c@proton.me>
2024-04-17 08:31:31 -0400

    vite5

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-//import legacy from '@vitejs/plugin-legacy'
+import legacy from '@vitejs/plugin-legacy'
+//from package.json: "@vitejs/plugin-legacy": "^5.3.2",
 
 
 //https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    /*legacy({
+    legacy({
       targets: ['defaults', 'not IE 11'],
-    }),*/
+    }),
     react()
   ],
 })
