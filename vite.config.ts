1b05088e6b9503dd5ef5b3e1a1d61274fab6555f
aeonix-eth <a17c@proton.me>
2024-04-18 07:13:56 -0400

    8

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from "vite-plugin-singlefile"
 import legacy from '@vitejs/plugin-legacy'
 //from package.json: "@vitejs/plugin-legacy": "^5.3.2",
 
@@ -8,8 +9,9 @@ import legacy from '@vitejs/plugin-legacy'
 export default defineConfig({
   plugins: [
     legacy({
-      targets: ['defaults', 'Firefox <= 60'],
+      targets: ['defaults', 'since 2015'],
     }),
-    react()
+    react(),
+    viteSingleFile()
   ],
 })
