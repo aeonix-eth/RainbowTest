2945f07dfbedad76ecc4135a723d5fcd6d5cdae5
aeonix-eth <a17c@proton.me>
2024-04-18 07:18:00 -0400

    9

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteSingleFile } from "vite-plugin-singlefile"
-import legacy from '@vitejs/plugin-legacy'
+//import legacy from '@vitejs/plugin-legacy'
+//legacy({
+//  targets: ['defaults', 'since 2015'],
+//}),
 //from package.json: "@vitejs/plugin-legacy": "^5.3.2",
 
 
 //https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    legacy({
-      targets: ['defaults', 'since 2015'],
-    }),
     react(),
     viteSingleFile()
   ],
